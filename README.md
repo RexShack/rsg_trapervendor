03d715a9c95aada70f812085be8b08ed658e94ba
RexShack <rexshackuk@gmail.com>
2022-05-23 22:09:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# RexshackGaming
+- discord : https://discord.gg/s5uSk56B65
+- youtube : https://www.youtube.com/channel/UCikEgGfXO-HCPxV5rYHEVbA
+- github : https://github.com/RexShack
+
+# Framework QBCore RedM Edition
+- https://github.com/qbcore-redm-framework
+
+# Dependancies
+- qbr-core : https://github.com/qbcore-redm-framework
+- rsg_notify : https://github.com/RexShack/rsg_notify
+
+# Installation
+- ensure that the dependancies are added and started
+- add rsg_trapervendor to your resources folder
+
+# Starting the resource
+- add the following to your server.cfg file : ensure rsg_trapervendor
+
+# Note
+- if anyone knows a better way of doing this please share
+
+# Support
+- discord : https://discord.gg/s5uSk56B65
+- email : rexshackuk@gmail.com
---
+++ client/client.lua
@@ -0,0 +1,44 @@
+-- Trapper-Vendor-Valentine
+Citizen.CreateThread(function()
+	exports['qbr-core']:createPrompt('Trapper-Vendor-Valentine', vector3(-336.6282, 772.65386, 116.2103), 0xF3830D8E, 'Valentine Trapper Vendor', {
+		type = 'server',
+		event = 'rsg_trappervendor:server:sell',
+		args = { false, true, false },
+	})
+end)
+
+-- Trapper-Vendor-StDenis
+Citizen.CreateThread(function()
+	exports['qbr-core']:createPrompt('Trapper-Vendor-StDenis', vector3(2832.3007, -1225.554, 47.657634), 0xF3830D8E, 'Saint Denis Trapper Vendor', {
+		type = 'server',
+		event = 'rsg_trappervendor:server:sell',
+		args = { false, true, false },
+	})
+end)
+
+-- Trapper-Vendor-RiggsStation
+Citizen.CreateThread(function()
+	exports['qbr-core']:createPrompt('Trapper-Vendor-RiggsStation', vector3(-1006.938, -549.3896, 99.393592), 0xF3830D8E, 'Riggs Station Trapper Vendor', {
+		type = 'server',
+		event = 'rsg_trappervendor:server:sell',
+		args = { false, true, false },
+	})
+end)
+
+-- Trapper-Vendor-WestElizabeth
+Citizen.CreateThread(function()
+	exports['qbr-core']:createPrompt('Trapper-Vendor-WestElizabeth', vector3(-2843.525, 142.12048, 184.59883), 0xF3830D8E, 'West Elizabeth Trapper Vendor', {
+		type = 'server',
+		event = 'rsg_trappervendor:server:sell',
+		args = { false, true, false },
+	})
+end)
+
+-- Trapper-Vendor-Stawberry
+Citizen.CreateThread(function()
+	exports['qbr-core']:createPrompt('Trapper-Vendor-Stawberry', vector3(-1746.238, -388.9359, 156.57376), 0xF3830D8E, 'Stawberry Trapper Vendor', {
+		type = 'server',
+		event = 'rsg_trappervendor:server:sell',
+		args = { false, true, false },
+	})
+end)
